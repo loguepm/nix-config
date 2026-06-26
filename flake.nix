1e4d4d3789808b2d56f5702b892e21a4e102c15c
loguepm <public@logue.pm>
2026-06-26 18:35:36 +0200

    Create machine directory

--- flake.nix
+++ flake.nix
@@ -34,43 +34,15 @@
             };
           }
         );
-
-      configuration = { pkgs, ... }: {
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-        environment.systemPackages = [
-          pkgs.vim
-        ];
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        # Enable alternative shell support in nix-darwin.
-        # programs.fish.enable = true;
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 6;
-
-        nix.enable = false;
-
-        programs.direnv.enable = true;
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-      };
     in
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#persephone
       darwinConfigurations."persephone" = nix-darwin.lib.darwinSystem {
-        modules = [ configuration ];
+        modules = [ ./machines/persephone.nix ];
       };
 
-      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt-tree);
 
       devShells = forEachSupportedSystem (
         { pkgs, system }:
@@ -78,6 +50,8 @@
           default = pkgs.mkShellNoCC {
             packages = with pkgs; [
               self.formatter.${system}
+
+              nixd
             ];
           };
         }
