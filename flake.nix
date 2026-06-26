4fec949bb83fe853e3e00a34b4af8fc927f425a2
loguepm <public@logue.pm>
2026-06-26 17:59:59 +0200

    Install nix-darwin

--- flake.nix
+++ flake.nix
@@ -1,10 +1,18 @@
 {
-  description = "A minimal flake template that you can adapt to your own project";
+  description = "Example nix-darwin system flake";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs =
-    { self, ... }@inputs:
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
     let
       inherit (inputs.nixpkgs) lib;
 
@@ -26,18 +34,38 @@
             };
           }
         );
+
+      configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = [
+          pkgs.vim
+        ];
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        nix.enable = false;
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
     in
     {
-      devShells = forEachSupportedSystem (
-        { pkgs, system }:
-        {
-          default = pkgs.mkShellNoCC {
-            packages = with pkgs; [
-              self.formatter.${system}
-            ];
-          };
-        }
-      );
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#persephone
+      darwinConfigurations."persephone" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
 
       formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
     };
